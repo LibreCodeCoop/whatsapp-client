d2117ce27b717ae3dd4886a8d72245de7e297ee7
Vitor Mattos <vitor@php.rio>
2020-08-29 10:22:23 -0300

    Implement log Fix restore session flow

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,9 @@ use Closure;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverCapabilities;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogHandler;
+use Monolog\Logger;
 use Symfony\Component\Panther\Client as PantherClient;
 use WhatsappClient\webwhatsapi\Wrapper;
 
@@ -23,6 +26,10 @@ class Client
      * @var Wrapper
      */
     public $WsapiWrapper;
+    /**
+     * @var Logger
+     */
+    protected $logger;
     public function __construct($host, ?WebDriverCapabilities $capabilities = null, $sessionFile = '.session')
     {
         if (!$capabilities) {
@@ -31,6 +38,35 @@ class Client
         }
         $this->sessionFile = $sessionFile;
         $this->setClient(PantherClient::createSeleniumClient($host, $capabilities));
+        $this->enableLog();
+    }
+
+    public function enableLog(array $settings = [])
+    {
+        $defaultSettings = [
+            'name' => 'WHATSAPP',
+            'handlers' => [
+                new StreamHandler($_ENV['LOG_DIR'] . '/log.log'),
+                new SyslogHandler(true)
+            ],
+            'enable_error_handler' => true
+        ];
+        $settings = array_merge($defaultSettings, $settings);
+        if ($settings['logger']) {
+            $this->logger = $settings['logger'];
+        } else {
+            $this->logger = new Logger($settings['name']);
+        }
+        foreach ($settings['handlers'] as $handler) {
+            $this->logger->pushHandler($handler);
+        }
+        if ($settings['enable_error_handler']) {
+            $handler = new \Monolog\ErrorHandler($this->logger);
+            $handler->registerErrorHandler([], false);
+            $handler->registerExceptionHandler();
+            $handler->registerFatalHandler();
+        }
+        return $this->logger;
     }
 
     /**
@@ -51,13 +87,17 @@ class Client
         if (!$this->loadSessionFronFile()) {
             $this->login();
         }
+        $this->WsapiWrapper = new Wrapper($this->client);
     }
 
-    public function observer()
+    public function loop($callback)
     {
         $this->running = true;
         while ($this->running) {
             $jsMessages = $this->WsapiWrapper->getBufferedNewMessages();
+            if ($jsMessages) {
+                $callback($this, $jsMessages);
+            }
             sleep(2);
         }
     }
@@ -65,10 +105,12 @@ class Client
     private function loadSessionFronFile()
     {
         if (!file_exists($this->sessionFile)) {
+            $this->logger->info('Session file does not exist: ' . $this->sessionFile);
             return false;
         }
         $json = file_get_contents($this->sessionFile);
         if (!$json) {
+            $this->logger->info('Empty session file: ' . $this->sessionFile);
             return false;
         }
         $this->client->request('GET', 'https://web.whatsapp.com/');
@@ -81,10 +123,24 @@ class Client
             SCRIPT
         );
         $this->client->request('GET', 'https://web.whatsapp.com/');
-        try {
-            $menu = $this->client->findElement(WebDriverBy::cssSelector('[data-testid="menu"][data-icon="menu"]'));
-        } catch (\Exception $e) { }
-        return !empty($menu);
+        do {
+            $noPopup = null;
+            try {
+                $menu = $this->client->findElement(WebDriverBy::cssSelector('[data-testid="menu"][data-icon="menu"]'));
+            } catch (\Exception $e) {
+                try {
+                    $popup = $this->client->findElement(WebDriverBy::cssSelector('[data-animate-modal-body="true"]'));
+                    $errormsg = explode("\n", $popup->getText())[0];
+                    $this->logger->info('Failure on load session: ' . $errormsg);
+                } catch (\Exception $noPopup) { }
+            }
+        } while(!empty($noPopup) || $errormsg);
+        if (empty($menu)) {
+            $this->logger->info('Invalid session');
+            return false;
+        }
+        $this->logger->info('Session loadded');
+        return true;
     }
 
     private function login()
@@ -104,7 +160,6 @@ class Client
             } catch (\Exception $e) { }
             try {
                 $menu = $this->client->findElement(WebDriverBy::cssSelector('[data-testid="menu"][data-icon="menu"]'));
-                $this->WsapiWrapper = new Wrapper($this->client);
             } catch (\Exception $e) { }
             sleep(1);
         } while(empty($menu));
